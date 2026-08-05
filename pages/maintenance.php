0d8974b66aa85536da5d960f49152336f8a07967
Habiba-bob153 <habibaabdalghany24@gmail.com>
2026-08-05 17:55:45 +0300

    fix

---
+++ pages/maintenance.php
@@ -0,0 +1,17 @@
+ <?php include("../includes/header.php"); 
+?>
+<?php include("../includes/sidebar.php"); ?>
+<link rel="stylesheet" href="/GREENNILE-CITY/assets/css/maintenance.css">
+
+
+
+
+
+
+
+
+
+
+ <script src="/GREENNILE-CITY/assets/js/maintenance.js"></script>
+
+<?php include("../includes/footer.php"); ?>
