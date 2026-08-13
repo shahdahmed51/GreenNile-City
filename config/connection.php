a628cefdd2ed4731e980fafb7cb040f4772ddaf0
Habiba-bob153 <habibaabdalghany24@gmail.com>
2026-08-13 05:57:00 +0300

    شيي

--- config/connection.php
+++ config/connection.php
@@ -3,12 +3,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-<<<<<<< HEAD
-$database = "greennile";   
-=======
-$database = "greennile.sql";
-
->>>>>>> a5f7e0c5a852f3372e2e1ddfcd44bd77c78344c1
+$database = "greennile"; 
 $conn = mysqli_connect($host, $username, $password, $database);
 
 if (!$conn) {
